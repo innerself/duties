91b17a9f6beb123acabdd5ca43d1bc8d40b26b54
Sergey Petrin <spetrin@gmail.com>
2021-07-11 21:13:50 +0300

    Add basic test

--- duties/tests.py
+++ duties/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginTestCase(TestCase):
+    def test_page_opens(self):
+        response = self.client.get(reverse('duties:login'))
+        self.assertEqual(response.status_code, 200)
