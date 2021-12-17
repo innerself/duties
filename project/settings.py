390e160eae86615d7cff0374d7c0ae34beff0fe7
Sergey Petrin <spetrin@gmail.com>
2021-12-17 23:28:08 +0300

    Add console logging

--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,21 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
